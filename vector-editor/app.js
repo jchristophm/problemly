e9b697e0a484ec0cdbb5c29864dda3f42d939b0e
jchristophm <jchristophm@gmail.com>
2025-05-28 14:16:22 -0500

    Update app.js

--- vector-editor/app.js
+++ vector-editor/app.js
@@ -537,10 +537,6 @@ window.addDashedArrow = function () {
   layer.draw();
 };
 
-window.addEquation = function () {
-  openMathModal();
-};
-
 // ============== Utility ==============
 
 function createHandle(x, y, onDragMove) {
@@ -764,4 +760,8 @@ function insertEquation() {
     img.src = dataURL;
   });
 }
+
+window.addEquation = function () {
+  openMathModal();
+};
 
